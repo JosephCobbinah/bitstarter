9964222188138b4c53aa66e6f363d3995e1ec037
Joseph Cobbinah <joseph@ubuntu.(none)>
2013-07-08 03:00:11 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
 var fs = require('fs');
 var buf = new buffer(fs.readFileSync("index.html", "utf-8"));
+app.get('/', function(request, response) {
+
 
   response.send(buf.toString());
 });
