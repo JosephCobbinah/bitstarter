d9e611d623905b44a31ed2fb1c037c259ef9efb3
Joseph Cobbinah <joseph@ubuntu.(none)>
2013-07-08 02:09:50 -0700

    Updated

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 var fs=require('fs');
 var buf=new buffer();
-  response.send(buf.toString(fs.readfilesync("index.html", "utf-8")));
+  response.send(buf.toString('utc 8',fs.readfilesync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
